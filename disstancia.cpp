92259d81465fd83835863894887069e08226bd55
Sarai75 <moaj080602mmcrlsa3@cbtis217.edu.mx>
2024-10-14 08:03:04 -0600

    Se agrego el autor

---
+++ disstancia.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+    // Función para calcular la distancia entre dos puntos
+    double calcularDistancia(double x1, double y1, double x2, double y2)
+    {
+    return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    }
+
+int main() {
+    double x1, y1, x2, y2;
+
+    cout << "Pedir al usuario que ingrese las coordenadas\n";
+    cout << "Aut@r: Sarai Morales" << endl;
+    cout << "x1: ";
+    cin >> x1;
+    cout << "y1: ";
+    cin >> y1;
+    cout << "x2: ";
+    cin >> x2;
+    cout << "y2: ";
+    cin >> y2;
+
+    // Calcular la distancia
+    double distancia = calcularDistancia(x1, y1, x2, y2);
+
+    // Mostrar el resultado
+    cout << "La distancia entre los puntos (" << x1 << ", " << y1 << ") y ("
+         << x2 << ", " << y2 << ") es: " << distancia << endl;
+
+    return 0;
+}
